041eaf78a92266aa5c80b659b26e3579caff3eab
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-05-28 15:28:10 +0200

    feat: cache for all phones

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class PhoneController extends AbstractController
 {
@@ -23,18 +25,24 @@ class PhoneController extends AbstractController
 
     // Function to return all phones in the database with a JSON response
     #[Route('/api/phones', name: 'app_phones', methods: ['GET'])]
-    public function getPhones(Request $request, PhoneRepository $phoneRepository, SerializerInterface $serializer): JsonResponse
+    public function getPhones(Request $request, PhoneRepository $phoneRepository, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
     {
         // extract the page number and limit from the request body
         $content = json_decode($request->getContent(), true);
         $page = $content['page'] ?? 1;
         $limit = $content['limit'] ?? 10;
 
-        $phones = $phoneRepository->findAllWithPagination($page, $limit);
+        $idCache = "getAllPhones_{$page}_{$limit}";
 
-        $jsonPhones = $serializer->serialize($phones, 'json');
+        $phoneList = $cache->get($idCache, function (ItemInterface $item) use ($phoneRepository, $page, $limit) {
+            echo ("Cache miss for the phone list with page {$page} and limit {$limit}");
+            $item->tag('phones');
+            return $phoneRepository->findAllWithPagination($page, $limit);
+        });
 
-        return new JsonResponse($jsonPhones, Response::HTTP_OK, [], true);
+        $jsonPhoneList = $serializer->serialize($phoneList, 'json');
+
+        return new JsonResponse($jsonPhoneList, Response::HTTP_OK, [], true);
     }
 
     // Function to return a specific phone in the database with a JSON response
