47515c821746ac4b553446225ad258f124ccf3e1
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-06-01 16:04:31 +0200

    feat: test cache client

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Serializer\SerializerInterface;
 use App\Repository\UserRepository;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ClientController extends AbstractController
@@ -23,14 +25,11 @@ class ClientController extends AbstractController
 
     // Function to get all the users, which are the real users of the api, only accessible by the admin
     #[Route('/api/clients', name: 'app_clients', methods: ['GET'])]
-    public function getClients(Request $request, UserRepository $clientRepository, SerializerInterface $serializer): JsonResponse
+    public function getClients(Request $request, UserRepository $clientRepository, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
     {
         // Check if the current user has admin privileges
         if (!$this->isGranted('ROLE_ADMIN')) {
-            // Throw an access denied exception
-            // throw new AccessDeniedException('Unable to access this page, you are not an admin!');
             return new JsonResponse(['message' => 'Unable to access this page, you are not an admin!'], Response::HTTP_FORBIDDEN);
-    
         }
 
         // extract the page number and limit from the request body
@@ -38,11 +37,16 @@ class ClientController extends AbstractController
         $page = $content['page'] ?? 1;
         $limit = $content['limit'] ?? 10;
 
-        $clients = $clientRepository->findAllWithPagination($page, $limit);
+        $cacheKey = "getClients_{$page}_{$limit}";
 
+        $clients = $cache->get($cacheKey, function (ItemInterface $item) use ($clientRepository, $page, $limit) {
+            echo ("Cache miss for clients with page {$page} and limit {$limit} \n");
+            $item->tag('clients');
+            return $clientRepository->findAllWithPagination($page, $limit);
+        });
 
         $jsonClients = $serializer->serialize($clients, 'json', ['groups' => 'getClients']);
 
-        return new JsonResponse($jsonClients, Response::HTTP_OK, [], true);
+        return new JsonResponse($jsonClients, Response::HTTP_OK);
     }
 }
