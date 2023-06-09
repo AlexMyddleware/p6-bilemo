5accc3073d2e814c57a1e414a7759ef00b10e949
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-06-09 13:27:44 +0200

    feat: update get clienst
    use getInt to typecheck, add try catch, chec k for empty client list

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -34,33 +34,49 @@ class ClientController extends AbstractController
 
     // Function to get all the users, which are the real users of the api, only accessible by the admin
     #[Route('/api/clients', name: 'app_clients', methods: ['GET'])]
-    public function getClients(Request $request, UserRepository $clientRepository, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
-    {
+    public function getClients(
+        Request $request, 
+        UserRepository $clientRepository, 
+        SerializerInterface $serializer, 
+        TagAwareCacheInterface $cache
+    ): JsonResponse {
         // Check if the current user has admin privileges
         if (!$this->isGranted('ROLE_ADMIN')) {
             return new JsonResponse(['message' => 'Unable to access this page, you are not an admin!'], Response::HTTP_FORBIDDEN);
-    
         }
 
         $context = SerializationContext::create()->setGroups(['getClients']);
 
-        
         // extract the page number and limit from url parameters
-        $page = $request->query->get('page', 1);
-        $limit = $request->query->get('limit', 10);
+        $page = $request->query->getInt('page', 1);
+        $limit = $request->query->getInt('limit', 10);
+
+        if ($page <= 0 || $limit <= 0) {
+            return new JsonResponse(['message' => 'Page number and limit should be greater than 0'], Response::HTTP_BAD_REQUEST);
+        }
 
         $idCache = "getAllClients_{$page}_{$limit}";
 
-        $clientList = $cache->get($idCache, function (ItemInterface $item) use ($clientRepository, $page, $limit) {
-            $this->logger->info("Cache miss for the client list with page {$page} and limit {$limit}");
-            $item->tag('clients');
-            return $clientRepository->findAllWithPagination($page, $limit);
-        });
+        try {
+            $clientList = $cache->get($idCache, function (ItemInterface $item) use ($clientRepository, $page, $limit) {
+                $this->logger->info("Cache miss for the client list with page {$page} and limit {$limit}");
+                $item->tag('clients');
+                return $clientRepository->findAllWithPagination($page, $limit);
+            });
 
+            // Check if the client list is empty
+            if (empty($clientList)) {
+                return new JsonResponse(['message' => 'No clients found.'], Response::HTTP_NO_CONTENT);
+            }
 
-        $jsonClientList = $serializer->serialize($clientList, 'json', $context);
+            $jsonClientList = $serializer->serialize($clientList, 'json', $context);
 
-        return new JsonResponse($jsonClientList, Response::HTTP_OK, ['json_encode_options' => JSON_PRETTY_PRINT], true);
+            return new JsonResponse($jsonClientList, Response::HTTP_OK, ['json_encode_options' => JSON_PRETTY_PRINT], true);
+
+        } catch (\Exception $e) {
+            $this->logger->error("Error occurred while getting clients: {$e->getMessage()}");
+            return new JsonResponse(['message' => 'Internal server error'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     // Function to return a specific client in the database with a JSON response
